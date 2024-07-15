8b5ff5e78a45264b2e605d42984337e8606298ef
unknown <swati308mmu@gmail.com>
2024-07-15 11:31:27 +0000

    Final Commit ngrok Supported

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import streamlit as st
 
 from PIL import Image
 from pathlib import Path
+import base64
 
 if os.path.exists('runs'):
     shutil.rmtree('runs')
@@ -20,8 +21,28 @@ def save_uploaded_file(uploadedfile):
     with open(uploadedfile.name, "wb") as f:
         f.write(uploadedfile.getbuffer())
     return st.success(f"Saved file: {uploadedfile.name}")
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+def img_to_html(img_path):
+    img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+      img_to_bytes(img_path)
+    )
+    return img_html
 
-st.title("Upload and Save Video File")
+st.markdown("<p style='text-align: right; color: white;'> "+img_to_html('kpmg.png')+"</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: white;'> "+img_to_html('national_emblem_resized.png')+"</p>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: blue;'>Computer Vision</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: blue;'>Multi Object Detector and Tracker Demo</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: grey;'>KPMG DEMO</h3>", unsafe_allow_html=True)
+st.write("\n\n\n\n\n")
+st.write("\n\n\n\n\n")
+st.write("\n\n\n\n\n")
+
+st.markdown("<h2 style='text-align: center; color: blue;'>Yolov9 and Deepsort</h2>", unsafe_allow_html=True)
+#st.markdown("<h4 style='text-align: center; color: black;'>Acne and Actinic Skin Disease Classification</h4>", unsafe_allow_html=True)
+st.title("Upload and Save Video or Image File")
 
 # Upload the video file
 uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov", "mkv", "jpg", "jpeg", "png"])
